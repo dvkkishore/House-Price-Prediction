930ac10b88292798aedef8cfb60a2e94c4b2c3cc
DANNANA VENKATA KISHORE <72384182+dvkkishore@users.noreply.github.com>
2022-10-06 09:54:06 +0530

    Done

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+import pandas as pd
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+data = pd.read_csv("D:\MACHINE LEARNING\HOUSE PRICE PREDICTION\Cleaned_data.csv")
+pipe = pickle.load(open("D:\MACHINE LEARNING\HOUSE PRICE PREDICTION\RidgeModel1.pkl","rb"))
+
+@app.route('/')
+def index():
+    locations = sorted(data['location'].unique())
+    return render_template('index.html', locations = locations)
+
+@app.route('/predict', methods = ['POST'])
+def predict():
+    location = request.form.get('location')
+    bhk = request.form.get('bhk')
+    bath = request.form.get('bath')
+    sqft = request.form.get('total_sqft')
+
+    input = pd.DataFrame([[location,sqft,bath,bhk]],columns = ['location', 'total_sqft', 'bath', 'bhk'])
+
+    prediction = pipe.predict(input)[0]*1e5
+
+    return str(np.round(prediction,2))
+
+if __name__ == "__main__":
+    app.run(debug = True)   
